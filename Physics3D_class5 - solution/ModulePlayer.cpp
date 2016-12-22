dda88382110dd938313add4b4867914874cf27c0
Danny0ner <dlopez3a@gmail.com>
2016-12-22 16:54:18 +0100

    Map finished and timer added, rest to implement sound and change drone's form

--- Physics3D_class5 - solution/ModulePlayer.cpp
+++ Physics3D_class5 - solution/ModulePlayer.cpp
@@ -104,7 +104,7 @@ bool ModulePlayer::Start()
 
 	*/
 	vehicle = App->physics->AddVehicle(car);
-	vehicle->SetPos(0, 10, 0);
+	vehicle->SetPos(0, 2, 0);
 
 	/////////////////////////// helices//////////////////
 	c1.height = 0.01f;
@@ -332,9 +332,7 @@ update_status ModulePlayer::Update(float dt)
 	if (vehicle->GetKmh() < 10 && vehicle->GetKmh() > -10) lastdirec = nothing;
 	vehicle->Render();
 
-	char title[80];
-	sprintf_s(title, "%.1f Km/h", vehicle->GetKmh());
-	App->window->SetTitle(title);
+
 
 
 	return UPDATE_CONTINUE;
--- Physics3D_class5 - solution/ModuleSceneIntro.h
+++ Physics3D_class5 - solution/ModuleSceneIntro.h
@@ -22,9 +22,9 @@ public:
 	void OnCollision(PhysBody3D* body1, PhysBody3D* body2);
 
 public:
-	Cube s, Flooor, door6_1, door6_2, door6_3, door6_4, sensor1, sensor3;
+	Cube s, Flooor, door6_1, door6_2, door6_3, door6_4, sensor1, sensor3, sensor5, sensor6;
 	Cube ring1, ring2, ring3, ring4, ring5, ring6, ring7, ring8, ring9, ring10, ring11, ring12;
-	Cylinder start, door1l, door1r , door2palo,door5palo, door2circle, door4_1, door4_2, door4_3 , door7_1, door7_2, door7_3 , sensor2, sensor4;
+	Cylinder start, door1l, door1r , door2palo,door5palo, door2circle, door4_1, door4_2, door4_3 , door7_1, door7_2, door7_3 , sensor2, sensor4, end, sensor7;
 	Cube ring13, ring14, ring15, ring16, ring17, ring18, ring19, ring20, ring21, ring22, ring23, ring24;
 	
 	PhysBody3D* map[40];
@@ -32,5 +32,12 @@ public:
 	PhysBody3D* Door2;
 	PhysBody3D* Door3;
 	PhysBody3D* Door4;
+	PhysBody3D* Door5;
+	PhysBody3D* Door6;
+	PhysBody3D* Door7;
 	PhysBody3D* sensor;
+
+	uint time = 0, timepassed = 0;
+	bool started = false, finished = false;
+	bool check1 = false, check2 = false, check3 = false, check4 = false, check5 = false, check6 = false;
 };
